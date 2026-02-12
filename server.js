aa82988c63567010835db5e8e679af25c12a7c98
ik8sqi <ik8sqi@users.noreply.github.com>
2026-02-12 11:18:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const fs = require('fs');
 
 const jsonPayload = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
 
-fastify.all('/*', async (request, reply) => {
+const handler = async (request, reply) => {
   reply
     .header('Content-Type', 'application/json')
     .code(200)
     .send(jsonPayload);
-});
+};
+
+fastify.get('/', handler);
+fastify.get('/*', handler);
+fastify.post('/*', handler);
+fastify.put('/*', handler);
+fastify.delete('/*', handler);
+fastify.patch('/*', handler);
+fastify.options('/*', handler);
 
 const port = process.env.PORT || 10000;
 fastify.listen({ port, host: '0.0.0.0' }).catch(err => {
